0f1fff963bb04a5d24e1ba568dc193b6abb83fb0
Mohamed Mehdawy <mohammedmehdawy@gmail.com>
2022-05-18 07:31:03 +0200

    install theme

--- projects/forms.py
+++ projects/forms.py
@@ -1,6 +1,16 @@
 from django.forms import ModelForm
+from django import forms
 from .models import Project
 class ProjectForm(ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for key, field in  self.fields.items():
+            field.widget.attrs.update({"class": "input"})
+    
     class Meta:
         model = Project
         fields = "__all__"
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple
+        }
