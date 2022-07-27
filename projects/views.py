fa11920e74fded7f9b472b73574e58f84d2b8b27
Mohamed Mehdawy <mohammedmehdawy@gmail.com>
2022-07-27 20:32:18 +0200

    build logic for pagination

--- projects/views.py
+++ projects/views.py
@@ -1,14 +1,32 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse, resolve
 from django.http import HttpResponse
 from .models import Project
 from django.contrib.auth.decorators import login_required
 from .forms import ProjectForm
 from .utils import searchProjects
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 # Create your views here.
 
 def projects(request):
     search_query, projects = searchProjects(request)
+    
+    per_page = 3
+    current_page = 1
+    if request.GET.get("page"):
+        current_page = request.GET.get("page")
 
+    paginator = Paginator(projects, per_page)
+    try:
+        projects = paginator.page(current_page)
+    except PageNotAnInteger:
+        return redirect(f"{reverse('projects')}?page=1")
+    except EmptyPage as error:
+        if str(error) == "That page number is less than 1":
+            return redirect(f"{reverse('projects')}?page=1")
+        else:
+            last_page = paginator.num_pages
+            return redirect(f"{reverse('projects')}?page={last_page}")
     context = {
         "projects": projects,
         "search_query": search_query
