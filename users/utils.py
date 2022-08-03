2e1bf0e28f501280afd00ff426edb97f9bb25325
Mohamed Mehdawy <mohammedmehdawy@gmail.com>
2022-08-03 21:13:16 +0200

    change distinct() place to best performance

--- users/utils.py
+++ users/utils.py
@@ -51,7 +51,7 @@ def searchProfiles(request) -> tuple[str, list]:
     if request.GET.get("search_query"):
         search_query = request.GET.get("search_query")
     skills = Skill.objects.filter(name__icontains = search_query)
-    profiles = Profile.objects.distinct().filter(Q(user_name__icontains = search_query) | 
+    profiles = Profile.objects.filter(Q(user_name__icontains = search_query) | 
                                         Q(headline__icontains = search_query) |
-                                        Q(skill__in = skills))
+                                        Q(skill__in = skills)).distinct()
     return search_query, profiles
