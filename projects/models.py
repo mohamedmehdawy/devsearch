a43e8f850b697dbe23e4b479162bd8a999aaea1b
Mohamed Mehdawy <mohammedmehdawy@gmail.com>
2022-08-04 13:23:11 +0200

    git review front with back

--- projects/models.py
+++ projects/models.py
@@ -37,7 +37,7 @@ class Review(models.Model):
         ),
     ]
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
-    # owener
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     body = models.TextField(null=True, blank=True)
     value = models.CharField(max_length= 200,choices=VOTE_TYPE)
@@ -45,6 +45,9 @@ class Review(models.Model):
 
     def __str__(self):
         return self.value
+
+    class Meta:
+        unique_together = ['owner', 'project']
 
 
 class Tag(models.Model):
