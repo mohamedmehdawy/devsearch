14601d3ec7c6fb376a69719fb8a828e2ea0c5d7f
Mohamed Mehdawy <mohammedmehdawy@gmail.com>
2022-08-13 22:19:51 +0200

    send welcome message

--- project/settings.py
+++ project/settings.py
@@ -137,3 +137,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # login
 LOGIN_URL = "/login/"
+
+# for email
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "mohammedmehdawy@gmail.com"
+EMAIL_HOST_PASSWORD = "dfdafzjdwovgczoz"
--- users/signals.py
+++ users/signals.py
@@ -2,8 +2,11 @@ from django.contrib.auth.models import User
 from .models import Profile
 from django.dispatch.dispatcher import receiver
 from django.db.models.signals import post_save, post_delete
-
+from django.core.mail import send_mail
+from django.conf import settings
 # create profile
+
+
 @receiver(post_save, sender=User)
 def createProfile(sender, instance, created, **kwargs):
     """
@@ -11,20 +14,26 @@ def createProfile(sender, instance, created, **kwargs):
     """
     if created:
         profile = Profile(
-            user = instance,
-            name = instance.first_name,
-            user_name = instance.username,
-            email = instance.email
+            user=instance,
+            name=instance.first_name,
+            user_name=instance.username,
+            email=instance.email
         )
         profile.save()
+        subject = "welcome to devsearch"
+        body = f"welcome {profile.name} to devsearch, best website to connect with developers around the world"
+        send_mail(subject, body, settings.EMAIL_HOST_USER, [profile.email], False)
+
 
 # update user
+
+
 @receiver(post_save, sender=Profile)
 def updateUser(sender, instance, created, **kwargs):
     """
         update user when profile is updated
     """
-    
+
     if not created:
         user = instance.user
         user.username = instance.user_name
@@ -33,6 +42,8 @@ def updateUser(sender, instance, created, **kwargs):
         user.save()
 
 # delete user
+
+
 @receiver(post_delete, sender=Profile)
 def deleteUser(sender, instance, **kwargs):
     """
