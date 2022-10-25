7b509b8b33320066ed713fd475c1e4343e808321
Mohamed Mehdawy <mohammedmehdawy@gmail.com>
2022-10-26 01:52:43 +0200

    work with jwt

--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from .serializers import ProjectsSerializer
 from projects.models import Project
@@ -16,6 +17,7 @@ def getRoutes(request):
     return Response(routes)
 
 @api_view(["GET"])
+@permission_classes([IsAuthenticated])
 def getProjects(request):
     projects = Project.objects.all()
     serializer = ProjectsSerializer(projects, many=True)
