358a459230227c77072d08a2943ac3b511461551
Mohamed Mehdawy <mohammedmehdawy@gmail.com>
2022-12-14 03:51:46 +0200

    build vote events

--- Front-end-test/assets/script/master.js
+++ Front-end-test/assets/script/master.js
@@ -13,7 +13,7 @@ async function getProjects() {
 // build projects
 function buildProjects(data) {
     let projects = document.querySelector("#projects");
-
+    projects.innerHTML = "";
     for (let i = 0; i < data.length; i++) {
         projects.innerHTML += `
             <section class="project">
@@ -25,12 +25,42 @@ function buildProjects(data) {
                     <p>${data[i].description.substring(0, 150)}...</p>
                 </section>
                 <section class="vote">
-                    <button class="up">&#43;</button>
-                    <button class="down">&#8722;</button>
+                    <button class="up" data-value="up" data-project="${data[i].id}">&#43;</button>
+                    <button class="down" data-value="down" data-project="${data[i].id}">&#8722;</button>
 
                 </section>
             </section>
         `;
     }
+    voteEvents();
 }
+
+// voteEvents
+function voteEvents() {
+    const end_point = "http://127.0.0.1:8000/api/projects/vote/";
+    const buttons = document.querySelectorAll(".project .vote button");
+    
+    buttons.forEach(button => {
+        button.addEventListener("click", () => {
+            const project = button.dataset.project;
+            const value = button.dataset.value;
+            
+            fetch(`${end_point}${project}/`, {
+                method: "post",
+                headers: {
+                    'Content-Type': 'application/json',
+                    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjcxMDY2NDQ2LCJpYXQiOjE2NzA5ODAwNDYsImp0aSI6IjVjODA2ZGQ1ZmFiMDQ1YmY4OGMwNDgzNzA0NDBiMmM3IiwidXNlcl9pZCI6MX0.uHqIth72_emyK1JfJq7L7gudeZKfHFhUvOQPy05xajQ",
+                },
+                body: JSON.stringify({
+                    value,
+                    body: "test body"
+                })
+            }).then(response => response.json()).then(data => {
+                console.log(data)
+                getProjects();
+            })
+        })
+    })
+}
+
 getProjects();
